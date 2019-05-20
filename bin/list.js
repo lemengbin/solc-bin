0e04593f62085104b8eff0edebfd35ef8f67aad1
lemengbin <lemengbin@163.com>
2019-05-20 14:58:37 +0800

    Added compiler version v0.4.20+commit.58b6c547

---
+++ bin/list.js
@@ -0,0 +1,12 @@
+var soljsonSources = [
+  "soljson-v0.4.20+commit.58b6c547.js"
+];
+var soljsonReleases = {
+  "0.4.20": "soljson-v0.4.20+commit.58b6c547.js"
+};
+
+if (typeof(module) !== 'undefined')
+  module.exports = {
+    'allVersions': soljsonSources,
+    'releases': soljsonReleases
+  };

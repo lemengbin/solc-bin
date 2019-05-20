564d968e248d7b2774b66d1a8a79c4ff3c9a7227
lemengbin <lemengbin@163.com>
2019-05-20 18:06:38 +0800

    Added compiler version v0.5.8+commit

--- bin/list.js
+++ bin/list.js
@@ -1,7 +1,9 @@
 var soljsonSources = [
+  "soljson-v0.5.8+commit.23d335f2.js",
   "soljson-v0.4.20+commit.58b6c547.js"
 ];
 var soljsonReleases = {
+  "0.5.8": "soljson-v0.5.8+commit.23d335f2.js",
   "0.4.20": "soljson-v0.4.20+commit.58b6c547.js"
 };
 
